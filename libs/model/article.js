8d5dac645e52f47100be3cddb8ccbfabe58bfa30
Maxime <maxime_m@live.ca>
2017-03-12 09:06:24 -0400

    beurk la france

--- libs/model/article.js
+++ libs/model/article.js
@@ -1,13 +1,13 @@
 module.exports = {
-	
-	getDistancesForStop: function (stopId, maxTime,  callback) {
+
+	getDistancesForStop: function (stopId, maxTime, callback) {
 		const gtfs = require('gtfs');
 		var Sync = require('sync');
 		const mongoose = require('mongoose');
 		mongoose.Promise = global.Promise;
-		
 
-		let getStopIndex = function(stopTimes){
+
+		let getStopIndex = function (stopTimes) {
 			return stopTimes.findIndex(stopTime => stopTime.stop_id == stopId);
 		}
 
@@ -21,7 +21,7 @@ module.exports = {
 
 			let trip = gtfs.getTripsByRouteAndDirection.sync(null, "stmBusMetro", route.route_id, direction.direction_id)[0];
 
-			let stopTimes = gtfs.getStoptimesByTrip.sync(null,"stmBusMetro", trip.trip_id);
+			let stopTimes = gtfs.getStoptimesByTrip.sync(null, "stmBusMetro", trip.trip_id);
 
 			let stopsBefore = stopTimes.splice(0, getStopIndex(stopTimes, stop));
 			let stopsAfter = [];
@@ -39,17 +39,23 @@ module.exports = {
 		var Sync = require('sync');
 
 		Sync(function () {
-			let stops = gtfs.getStopsByDistance.sync(null, lat, lng, .35);
-			console.log(stops);
+			let stops = gtfs.getStopsByDistance.sync(null, lat, lng, .1);
 
 			stops = stops
-			.filter(stop => 
-				/\/bus$/.test(stop.stop_url) //why would you do it this way?
-			)
-			.map(stop => 
-			{ 
-				return {nom:stop.stop_name, id: stop.stop_id, lat:stop.stop_lat, lng: stop.stop_lon}
-			});
+				.filter(stop =>
+					/\/bus$/.test(stop.stop_url) //why would you do it this way?
+				)
+				.map(stop => {
+					try {
+
+						let route = gtfs.getRoutesByStop.sync(null, "stmBusMetro", stop.stop_id)[0];
+						return { nom: route.route_short_name, id: stop.stop_id, lat: stop.stop_lat, lng: stop.stop_lon }
+						
+					} catch (error) {
+						console.log(error);
+						return {nom:error};
+					}
+				});
 			console.log(stops);
 			callback(stops);
 		});
@@ -66,13 +72,32 @@ module.exports = {
 			console.log(stops);
 
 			stops = stops
-			.filter(stop => 
-				!/\/bus$/.test(stop.stop_url) //why would you do it this way?
-			)
-			.map(stop => 
-			{ 
-				return {nom:stop.stop_name, id: stop.stop_id, lat:stop.stop_lat, lng: stop.stop_lon}
-			});
+				.filter(stop =>
+					!/\/bus$/.test(stop.stop_url) //why would you do it this way?
+				)
+				.map(stop => {
+					return { nom: stop.stop_name, id: stop.stop_id, lat: stop.stop_lat, lng: stop.stop_lon }
+				});
+
+			let distances;
+
+			// let calcDist = (stop) => {
+			// 	var R = 6371e3; // metres
+			// 	const pi = 3.14159;
+			// 	var φ1 = lat * (pi/180);
+			// 	var φ2 = stop.stop_lat * (pi/180);
+			// 	var Δφ = (stop.lat - lat) * (pi/180);
+			// 	var Δλ = (stop.lng - lng) * (pi/180);
+
+			// 	var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+			// 		Math.cos(φ1) * Math.cos(φ2) *
+			// 		Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+			// 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+			// 	var d = R * c;
+			// }
+
+			// distances = stops.map(stop => calcDist(stop));
 
 			callback(stops);
 		});
--- libs/routes/articles.js
+++ libs/routes/articles.js
@@ -1,84 +1,110 @@
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
-
+var async = require('async');
 var libs = process.cwd() + '/libs/';
 var log = require(libs + 'log')(module);
 
 var db = require(libs + 'db/mongoose');
 var stm = require(libs + 'model/article');
 
-router.get('/stops/:lat/:lng', function(req, res) {
-	stm.getStopsForPosition(req.param("lat"),req.param("lng"), (result) => {res.json(result); res.end()})
+router.get('/stops/:lat/:lng', function (req, res) {
+	stm.getStopsForPosition(req.param("lat"), req.param("lng"), (result) => { res.json(result); res.end() })
 });
 
-router.get('/distance/:stopId', function(req, res){
-	stm.getDistancesForStop(req.param("stopId"), 30, (result) => {res.json(result); res.end()})
+router.get('/distance/:stopId', function (req, res) {
+	stm.getDistancesForStop(req.param("stopId"), 30, (result) => { res.json(result); res.end() })
 });
 
-router.get('/superGoodUrl/:lat/:lng', function(req, res){
+router.get('/superGoodUrl/:lat/:lng', function (req, res) {
+
+	let buses = undefined, metro = undefined;
+	let result = {};
+	result.ligne = [];
+	async.series(
+		[
+			function (callback) {
+				stm.getStopsForPosition(req.param("lat"), req.param("lng"), result => callback(null, result));
+			},
+			function (callback) {
+				stm.getMetroForPosition(req.param("lat"), req.param("lng"), result => callback(null, result));
+			},
+			function (callback) {
+				callback(null, null);
+			}
+		],
+		function (err, r) {
+			buses = r[0];
+			metro = r[1];
+			
+				res.json(
+					{
+						bus: {
+							ligne: buses.map(result => {
+								return {
+									stop: { lat: result.lat, lng: result.lng },
+									nom: result.nom
+								};
+							})
+						},
+						metro: { ligne: metro.map(result => {
+								return {
+									stop: { lat: result.lat, lng: result.lng },
+									nom: result.nom
+								};
+							}) }
+					});
+				res.end();
+			}
+		
+	);
+
+})
+
+router.get('/bus/:lat/:lng', function (req, res) {
 	let result = {};
 	result.ligne = [];
 
-	stm.getStopsForPosition(req.param("lat"),req.param("lng"),
-	 results => {
-		res.json(
-			{
-				bus:{
-					ligne: results.map(result =>
-					 {
-						return {
-							stop: {lat:result.lat, lng: result.lng}, 
-							nom: result.nom};
+	stm.getStopsForPosition(req.param("lat"), req.param("lng"),
+		results => {
+			res.json(
+				{
+					bus: {
+						ligne: results.map(result => {
+							return {
+								stop: { lat: result.lat, lng: result.lng },
+								nom: result.nom
+							};
 						})
-					}, 
-					metro: {ligne:[]}
+
+					}
 				});
-		res.end();
-	})
+			res.end();
+		})
 })
 
-router.get('/bus/:lat/:lng', function(req, res){
-	let result = {};
-	result.ligne = [];
+router.get('/metro/:lat/:lng', function (req, res) {
 
-	stm.getStopsForPosition(req.param("lat"),req.param("lng"),
-	 results => {
-		res.json(
-			{
-					ligne: results.map(result =>
-					 {
-						return {
-							stop: {lat:result.lat, lng: result.lng}, 
-							nom: result.nom};
+	stm.getMetroForPosition(req.param("lat"), req.param("lng"),
+		results => {
+			res.json(
+				{
+					metro: {
+						ligne: results.map(result => {
+							return {
+								stop: { lat: result.lat, lng: result.lng },
+								nom: result.nom
+							};
 						})
-					
+					}
 				});
-		res.end();
-	})
-})
-
-router.get('/metro/:lat/:lng', function(req, res){
-
-	stm.getMetroForPosition(req.param("lat"),req.param("lng"),
-	 results => {
-		res.json(
-			{
-					ligne: results.map(result =>
-					 {
-						return {
-							stop: {lat:result.lat, lng: result.lng}, 
-							nom: result.nom};
-						})
-					
-				});
-		res.end();
-	})
+			res.end();
+		})
 })
 
 
-router.post('/', function(req, res) {
-	
+router.post('/', function (req, res) {
+
 	var article = new stm({
 		title: req.body.title,
 		author: req.body.author,
@@ -89,66 +115,66 @@ router.post('/', function(req, res) {
 	article.save(function (err) {
 		if (!err) {
 			log.info("New article created with id: %s", article.id);
-			return res.json({ 
-				status: 'OK', 
-				article:article 
+			return res.json({
+				status: 'OK',
+				article: article
 			});
 		} else {
-			if(err.name === 'ValidationError') {
+			if (err.name === 'ValidationError') {
 				res.statusCode = 400;
-				res.json({ 
-					error: 'Validation error' 
+				res.json({
+					error: 'Validation error'
 				});
 			} else {
 				res.statusCode = 500;
-				
+
 				log.error('Internal error(%d): %s', res.statusCode, err.message);
-				
-				res.json({ 
-					error: 'Server error' 
+
+				res.json({
+					error: 'Server error'
 				});
 			}
 		}
 	});
 });
 
-router.get('/:id', function(req, res) {
-	
+router.get('/:id', function (req, res) {
+
 	stm.findById(req.params.id, function (err, article) {
-		
-		if(!article) {
+
+		if (!article) {
 			res.statusCode = 404;
-			
-			return res.json({ 
-				error: 'Not found' 
+
+			return res.json({
+				error: 'Not found'
 			});
 		}
-		
+
 		if (!err) {
-			return res.json({ 
-				status: 'OK', 
-				article:article 
+			return res.json({
+				status: 'OK',
+				article: article
 			});
 		} else {
 			res.statusCode = 500;
-			log.error('Internal error(%d): %s',res.statusCode,err.message);
-			
-			return res.json({ 
-				error: 'Server error' 
+			log.error('Internal error(%d): %s', res.statusCode, err.message);
+
+			return res.json({
+				error: 'Server error'
 			});
 		}
 	});
 });
 
-router.put('/:id', function (req, res){
+router.put('/:id', function (req, res) {
 	var articleId = req.params.id;
 
 	stm.findById(articleId, function (err, article) {
-		if(!article) {
+		if (!article) {
 			res.statusCode = 404;
 			log.error('Article with id: %s Not Found', articleId);
-			return res.json({ 
-				error: 'Not found' 
+			return res.json({
+				error: 'Not found'
 			});
 		}
 
@@ -156,25 +182,25 @@ router.put('/:id', function (req, res){
 		article.description = req.body.description;
 		article.author = req.body.author;
 		article.images = req.body.images;
-		
+
 		article.save(function (err) {
 			if (!err) {
 				log.info("Article with id: %s updated", article.id);
-				return res.json({ 
-					status: 'OK', 
-					article:article 
+				return res.json({
+					status: 'OK',
+					article: article
 				});
 			} else {
-				if(err.name === 'ValidationError') {
+				if (err.name === 'ValidationError') {
 					res.statusCode = 400;
-					return res.json({ 
-						error: 'Validation error' 
+					return res.json({
+						error: 'Validation error'
 					});
 				} else {
 					res.statusCode = 500;
-					
-					return res.json({ 
-						error: 'Server error' 
+
+					return res.json({
+						error: 'Server error'
 					});
 				}
 			}
